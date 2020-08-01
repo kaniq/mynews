f4cc970107002a34d36d40d05741b249733ddd44
Mizuki <kaniqq@Yahoo.co.jp>
2020-08-01 11:51:29 +0000

    news Migration

---
+++ database/migrations/2020_08_01_113938_create_news_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewsTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title'); 
+            $table->string('body');  
+            $table->string('image_path')->nullable();  
+            $table->timestamps();
+        });
+    }
+
+
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}
